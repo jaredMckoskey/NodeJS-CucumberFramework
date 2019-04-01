310ebfc03b5fbde32de84b6943bd1bb4dba04e52
Jared Mckoskey <jmckoskey@gmail.com>
2019-04-01 09:18:10 -0500

    updateV1

--- src/utility/constants.js
+++ src/utility/constants.js
@@ -33,5 +33,31 @@ class Constants {
       return pagePath + product + "/";
     }
   }
+
+  getStateCode() {
+    let pagePath = process.cwd() + "/src/database/stateCodes.json";
+    const product = process.env.PRODUCT;
+    if (product === undefined) {
+    //TODO: Get correct product name from client
+      return pagePath + "";
+    } else {
+      return pagePath + product + "/";
+    }
+  }
+
+  todaysDate() {
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1; //January is 0!
+    var yyyy = today.getFullYear();
+
+    if(dd<10) {
+      dd = `0${dd}`;
+    } 
+    if(mm<10) {
+      mm = `0${mm}`;
+    }
+    return `${mm}/${dd}/${yyyy}`;
+  }
 }
 export default new Constants();
